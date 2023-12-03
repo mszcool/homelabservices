f6c294a93cfe1b225c6608a5c3d195b7ea9ebd5d
MarioSzp <marioszp@microsoft.com>
2023-12-03 20:48:01 +0100

    Adding ability to compile library on its own

---
+++ LibAssets/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <WiFiManager.h>
+
+#include "AssetApiBase.h"
+#include "AssetApiBaseData.h"
+#include "SecretHandler.h"
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
